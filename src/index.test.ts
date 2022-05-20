c96c3bd971bfa077fcada4c95f7d966ceb167c4e
Vincent Bailly <vibailly@microsoft.com>
2022-05-20 09:29:46 +0200

    make hash of circular deps independent of id

--- src/index.test.ts
+++ src/index.test.ts
@@ -203,4 +203,35 @@ describe("more complicated graph with circular dependencies", () => {
 
     expect(hash1).not.toEqual(hash2);
   });
+  it("ids in cycle does not matter", () => {
+    const graph1: Graph = {
+      nodes: [
+        { contentHash: "foo", id: 1 },
+        { contentHash: "bar", id: 2 },
+        { contentHash: "baz", id: 3 }
+      ],
+      links: [
+        { source: 1, target: 2 },
+        { source: 2, target: 3 },
+        { source: 3, target: 2 }
+      ],
+    };
+    const graph2: Graph = {
+      nodes: [
+        { contentHash: "foo", id: 1 },
+        { contentHash: "bar", id: 12 },
+        { contentHash: "baz", id: 13 }
+      ],
+      links: [
+        { source: 1, target: 12 },
+        { source: 12, target: 13 },
+        { source: 13, target: 12 }
+      ],
+    };
+
+    const hash1 = getNodeHashes(graph1).get(1);
+    const hash2 = getNodeHashes(graph2).get(1);
+
+    expect(hash1).toEqual(hash2);
+  });
 });
